ccc7a31521982dede1bfa7671344599649df236c
idontknowlolwhattoput <132071917+idontknowlolwhattoput@users.noreply.github.com>
2025-07-13 07:21:23 -0700

    dropdown part 1

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,15 @@
 import logo from '../assets/logo.svg'
+import { useState, useEffect } from 'react';
+import { FaSortDown, FaSortUp  } from "react-icons/fa";
 import '../components/navbar.css'
 export default function Navigation() {
 
+    // DROP DOWN
+    const [isDropDown, setDropDown] = useState(false);
+
+    // NAV SELECTION
+    const [selected, setSelected] = useState("")
+ 
     const NavSelection = [
         {
           name: "Home"
@@ -11,26 +19,42 @@ export default function Navigation() {
         },
         {
           name: "Services", 
-          img: ""
+          img: "True"
         },
 
     ]
 
-    return (
-        <div className="flex flex-row items-center justify-between shadow-2xl w-full h-24 px-3">
+    useEffect(() => {
+       
+    },[])
+    
+    
+    const handleDropDown = (selection) => {
+         setSelected(selection);
+         if (selection === "Services") {
+       setDropDown(prev => !prev);
+      }
+    };
 
+    return (
+        <div className="inter flex flex-row items-center justify-between shadow  w-full h-24 px-4 cursor-pointer">
            <div className="">
-             <img src={logo} className="w-24 h-16"/>
+            {/** <img src={logo} className="w-24 h-16"/> */} 
+             <h1 className="inter text-3xl font-bold">Diagnostic.a</h1>
            </div>
 
            <div className="">
-             <ul className="hidden md:flex">
+             <ul className="hidden md:flex ">
                 {NavSelection.map((nav, key) => (
-                   <li className="md:text-2xl px-4 font-semibold" key={key}>
-                      {nav.name}
+                   <li className="md:text-2xl px-7 font-light transition flex flex-row items-center" key={key} >
+                      <div className="flex flex-row gap-2" onClick={() => {handleDropDown(nav.name)}}>
+                         {nav.name}
+                         {nav.img && <FaSortDown className="w-7 h-7 " />}
+                      </div>
                    </li>
                 ))}
              </ul>
+               {isDropDown && (<div className="bg-red-500 w-5 h-5 absolute l-" ></div>)}
            </div>
 
            <div className="">
